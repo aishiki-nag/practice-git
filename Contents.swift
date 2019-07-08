b6051fc7605ecd29ac33ab7e9c9cac87680a2142
nimet <bethkaratas@icloud.com>
2019-07-08 11:54:16 -0700

    First Commit

---
+++ Contents.swift
@@ -0,0 +1,29 @@
+//import UIKit
+//
+//var name = "Sabyatha"
+// var name2 = "Nailah"
+//
+//print("Hi \(name) and \(name2)! We are so happy you're here ar KWK Portland!")
+
+// Ye Im a comment!Use me to keep track of codeeee!
+
+//Intention: I would like to create a piece of code that would take each scholar name and output each name into a string
+
+
+// Intention: Working with Intgers and Doubles
+// Data Types: Ints and Doubles
+
+
+var int = 4
+var double = 5.0
+
+print(Double (int) * double)
+
+var A = 12.4
+var B = 65
+var C = 31
+var D = 98
+var F = 4
+
+print(((A + B + C) Double (D))/4)
+
